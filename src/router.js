e7cb3f6cffbeadb3c8222935d2a84c18222e314b
Leonel Bazan <leo.bazan.x@gmail.com>
2020-09-29 02:21:35 -0300

    Router: Add '/private' endpoint to test verifyToken middleware

--- src/router.js
+++ src/router.js
@@ -2,13 +2,18 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require('./controllers/auth')
 const ProductController = require('./controllers/product')
+const authMiddleware = require('./middlewares/auth')
 
-router.get('/', (req, res) => {res.render('index.ejs')})
+router.get('/', (req, res) => { res.render('index.ejs') })
 
 router.post('/shop', ProductController.createProduct)
 
 router.post('/user', AuthController.signUp)
 
-router.get('*', (req, res) => {res.end("Error 404: Page not found")})
+router.get('/private', authMiddleware.verifyToken, (req, res) => {
+    res.send({ msg: 'You have access' })
+})
+
+router.get('*', (req, res) => { res.end("Error 404: Page not found") })
 
 module.exports = router
